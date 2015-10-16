3ce51ee1758057e65797b1a15fef90ca834013fd
nurelah55 <nur.elah@wgs.co.id>
2015-10-16 15:59:04 +0700

    add rule game

--- player.rb
+++ player.rb
@@ -1,12 +1,61 @@
 class Player
-	attr_accessor :name, :grade
-	def initialize
-		@name="hero"
-		@grade=4
+	attr_accessor :name, :manna, :blood
+	
+	def initialize (name, manna, blood)
+		@name= name
+		@manna= manna
+		@blood= blood
 	end
 
 end
 
-player = Player.new
-player.name
-player.name = "Nur elah"
+all_player = Array.new
+desc = 'new'
+while desc != 'exit'
+	puts "
+		# ============================== #
+		# Welcome to the Battle Arena #
+		# ------------------------------------------------- ---- #
+		# Description: #
+		# 1. type 'new' to create a character #
+		# 2. type 'start' to begin the fight #
+		# 3. type 'exit' to exit game #
+		# ------------------------------------------------- ---- #
+		# ------------------------------------------------- ---- #"
+		STDOUT.flush  
+		desc = gets.chomp
+
+	if desc.eql? "new"
+		puts " Who is the player ? "
+		STDOUT.flush  
+		player_name = gets.chomp
+		temp = Player.new(player_name, 100, 40)
+		all_player.push temp
+	elsif desc.eql? "start"
+		puts "All player are "
+		
+		 	all_player.each do |a|
+		 		puts 'Name : ' + a.name + " , Blood = #{a.blood}" + " , Manna = #{a.manna}" 
+		 		
+		 	end
+		    
+		 puts "Who will attack?"
+		 STDOUT.flush  
+		 attacker = gets.chomp
+		 all_player.each do |a|
+		 	 	if a.name.include?attacker
+		 			a.manna = a.manna - 10
+			 		puts 'Name : ' + a.name + " , Blood = #{a.blood}" + " , Manna = #{a.manna}" 
+			 		 		
+			 	else
+			 		a.blood = a.blood - 20
+			 		puts 'Name : ' + a.name + " , Blood = #{a.blood}" + " , Manna = #{a.manna}" 
+			 	end
+			 	if a.blood.eql? 0 
+			 		puts "Game Over"
+				end			
+		 end
+	else
+		puts "You type wrong keywoard"
+	end	
+end
